e35ce99d6ddc787fcd46ddc5cd3d6ec263b9855f
blackstar <blackpapermoonx32@gmail.com>
2016-02-11 00:11:31 +0800

    c-041

--- utility/collision.js
+++ utility/collision.js
@@ -3,6 +3,18 @@ var collision = (function(init) {
 	return init(V2D);
 })(function(V2D) {
 
+/**
+ * ============================================
+ */
+ 	function inRange(value, min, max) {
+ 		return value >= Math.min(min, max) && value <= Math.max(min, max);
+ 	}
+
+/**
+ * ============================================
+ */ 
+
+
 	function circleToCircle(c0, c1) {
 		return V2D.dist(c0.position, c1.position) <= c0.radius + c1.radius;
 	}
@@ -11,8 +23,14 @@ var collision = (function(init) {
 		return V2D.dist(c.position, p) < c.radius;
 	}
 
+	function rectToPoint(r, p) {
+		return inRange(p.x, r.position.x - r.width/2, (r.position.x - r.width/2) + r.width) 
+			&& inRange(p.y, r.position.y - r.height/2, (r.position.y - r.height/2) + r.height);
+	}
+
 	return {
 		circleToCircle: circleToCircle,
-		circleToPoint: circleToPoint
+		circleToPoint: circleToPoint,
+		rectToPoint: rectToPoint
 	};
 });
